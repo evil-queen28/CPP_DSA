414a97f4b6a55eed41f165b1712b7e501d15ef2f
evil-queen28 <vedikamandre28@gmail.com>
2022-01-03 11:14:53 +0530

    Added template

---
+++ Template/a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+#define pb push_back
+using namespace std;
+
+//Function for fast input output
+void i_o()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    i_o();
+    return 0;
+}
